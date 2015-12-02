399d4de37e09eaad6acea61ac86156f77d3a8dad
Daniel Brown <pi_anyone@hotmail.com>
2015-12-02 22:33:14 +0100

    increase max_iterations of test cases

--- Sudoku_Solver/test_puzzles.py
+++ Sudoku_Solver/test_puzzles.py
@@ -20,16 +20,16 @@ testgrid.solve(max_iters = 10, verbose = 0)
 
 testgrid = Grid.from_square_array(evilgrid_1)
 print "\n\nTest: 'evilgrid_1'"
-testgrid.solve(max_iters = 20, verbose = 0)
+testgrid.solve(max_iters = 30, verbose = 0)
 
 testgrid = Grid.from_string(evilgrid_2_str)
 print "\n\nTest: 'evilgrid_2_str'"
-testgrid.solve(max_iters = 20, verbose = 0)
+testgrid.solve(max_iters = 30, verbose = 0)
 
 testgrid = Grid.from_string(evilgrid_3_str)
 print "\n\nTest: 'evilgrid_3_str'"
-testgrid.solve(max_iters = 20, verbose = 0)
+testgrid.solve(max_iters = 30, verbose = 0)
 
 testgrid = Grid.from_string(evilgrid_4_str)
 print "\n\nTest: 'evilgrid_4_str'"
-testgrid.solve(max_iters = 20, verbose = 0)
+testgrid.solve(max_iters = 30, verbose = 0)
