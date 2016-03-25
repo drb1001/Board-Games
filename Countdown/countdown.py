a865e708fa8e2c964e8c21b52d1d1dee998ea650
Daniel Brown <dbrown@podio.com>
2016-03-25 09:59:14 +0000

    tidy print commands and comments

--- Countdown/countdown.py
+++ Countdown/countdown.py
@@ -4,7 +4,6 @@ import itertools
 large_numbers = [25,50,75,100]
 small_numbers = range(1,11)*2
 
-print selection
 # choose whether to pick random numbers, or to type in numbers, or a selection.
 large_count = raw_input("How many large numbers? (0-4): ")
 if large_count == "n":
@@ -18,38 +17,33 @@ else:
     selection = selection_large + selection_small
     target = random.choice(range(101,1000))
 
-target = random.choice(range(101,1000))
-print target
+print selection
+print "Your target is: %s" %str(target)
 
-# choose whether to pick random numbers, or to type in numbers.
-# wait for input (do an actual countdown)
 
 solutions = []
 # solver: operations are: + - * /
-
-#[50, 8, 4, 5, 1, 7]
-#894
+# todo: maybe try evolutionary example?
+#good examples:
+#[50, 8, 4, 5, 1, 7] -> 894
+#[7, 2, 1, 3, 6, 5] -> 738
 
 # generate permutations:
 selection = selection + [0]
 permutations = itertools.permutations(selection)
 perms = list(permutations)
-print len(perms)
 
 perms_new = []
 
 for p in perms:
     pos = p.index(0)
     temp = p[0:pos]
-    print p, "\t", temp
     perms_new.append(temp)
 
-print perms_new
-print len(perms_new)
-print len(set(perms_new))
+# print "permutations (`perms`) = %s" %str(len(perms))
+# print "amended permutations (`perms_new`) = %s" %str(len(perms_new))
+# print "unique permutations (`perms_new`) = %s" %str(len(set(perms_new)))
 
-# x + x + x + x + x + x + 0
-# 7^6*4^6
 
 
 # check each number
@@ -72,9 +66,5 @@ for x1 in selection:
 #     if x1 / x2 == target: solutions.append(str(x1)+"/"+str(x2))
 #     if x2 / x1 == target: solutions.append(str(x2)+"/"+str(x1))
 
-# x + x + x
-# x + x + x + x + x + x
 
 
-# print len(solutions)
-# print solutions
